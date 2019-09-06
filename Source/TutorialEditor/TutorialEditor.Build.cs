34e7a0e02dbfb14c6bdbe8b464fc2dc64cadf226
Forest Guhareza <arza.3d@gmail.com>
2019-09-07 06:29:17 +0700

    f

--- Source/TutorialEditor/TutorialEditor.Build.cs
+++ Source/TutorialEditor/TutorialEditor.Build.cs
@@ -8,20 +8,20 @@ public class TurorialEditor : ModuleRules
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-    PublicDependencyModuleNames.AddRange(new string[] { "Tutorial" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Tutorial" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
   //Only if you created the Public and Private folder paths
       PublicIncludePaths.AddRange(
-          new string[]
-          {
-              "TutorialEditor/Public"
-          });
+        new string[]
+        {
+          "TutorialEditor/Public"
+        });
 
       PrivateIncludePaths.AddRange(
-          new string[]
-          {
-          "TutorialEditor/Private"
-          });
+        new string[]
+        {
+            "TutorialEditor/Private"
+        });
 }
